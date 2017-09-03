a65b03274539ea68c40297b6584fabd9d8883a9f
Michael F. Herbst <info@michael-herbst.com>
2017-09-03 19:37:27 +0200

    Make script PEP8 compatible

--- emsl2yaml.py
+++ emsl2yaml.py
@@ -7,33 +7,36 @@ from datetime import datetime
 
 
 def emsl2yaml():
-  """ Download emsl basis data and return yaml string of it,
-  which is enhanced with metadata about the date and version
-  """
-  d = {
-    "list": emsl.download_basisset_list(),
-    "meta": {
-      # UTC timestamp
-      "timestamp": datetime.utcnow().isoformat(),
-      # yaml format version:
-      "version": "0.0.0",
+    """ Download emsl basis data and return yaml string of it,
+    which is enhanced with metadata about the date and version
+    """
+    d = {
+        "list": emsl.download_basisset_list(),
+        "meta": {
+            # UTC timestamp
+            "timestamp": datetime.utcnow().isoformat(),
+            # yaml format version:
+            "version": "0.0.0",
+        }
     }
-  }
-  return yaml.safe_dump(d)
+    return yaml.safe_dump(d)
+
 
 def main():
-  parser = argparse.ArgumentParser(
-    description="Download and convert the EMSL basis set exchange data to a yaml file")
-  parser.add_argument("output", type=str, metavar="out.yaml",
-                      help="Location to store the yaml file")
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser(
+        description="Download and convert the EMSL basis set exchange data to a yaml file"
+    )
+    parser.add_argument("output", type=str, metavar="out.yaml",
+                        help="Location to store the yaml file")
+    args = parser.parse_args()
 
-  _, ext = os.path.splitext(args.output)
-  if not ext in [ '.yml', '.yaml' ]:
-    args.output += '.yaml'
+    _, ext = os.path.splitext(args.output)
+    if ext not in ['.yml', '.yaml']:
+        args.output += '.yaml'
 
-  data = emsl2yaml()
-  open(args.output, "w").write(data)
+    data = emsl2yaml()
+    open(args.output, "w").write(data)
+
 
 if __name__ == "__main__":
-  main()
+    main()
