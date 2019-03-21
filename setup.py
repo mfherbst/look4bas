eb3c92fbbe66de71cd28b18ac7944fe0f13a07f7
Michael F. Herbst <info@michael-herbst.com>
2019-03-21 15:07:29 +0100

    Amend setup.py by pytest call

--- setup.py
+++ setup.py
@@ -1,13 +1,39 @@
 #!/usr/bin/env python3
 
 """Setup for look4bas"""
+import sys
+
 from setuptools import find_packages, setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = ""
+
+    def run_tests(self):
+        import shlex
+
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+
+        errno = pytest.main(shlex.split(self.pytest_args))
+        sys.exit(errno)
+
+
+#
+# Main setup code
+#
+long_description = """
+Command line tool to search for contracted Gaussian-type basis
+sets in electronic structure theory
+""".strip()
 
 setup(
     name='look4bas',
     description="Search for Gaussian basis sets",
-    long_description='Command line tool to search for contracted Gaussian-type basis '
-    'sets in electronic structure theory',
+    long_description=long_description,
     #
     url='https://github.com/mfherbst/look4bas',
     author='Michael F. Herbst',
@@ -24,6 +50,7 @@ setup(
         'beautifulsoup4 (>= 4.2)',
         'lxml (>= 4.2)'
     ],
+    tests_require=["pytest"],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
@@ -34,4 +61,6 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Operating System :: Unix',
     ],
+    #
+    cmdclass={"pytest": PyTest},
 )
